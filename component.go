90781ae7367434eb52cd9b3e261a48aa84c8d595
Geison Biazus <geisonbiazus@gmail.com>
2018-06-26 20:35:32 -0300

    Remove rendering logic from Component

--- component.go
+++ component.go
@@ -1,17 +1,5 @@
 package templatemanager
 
-import (
-	"bytes"
-	"strings"
-	"text/template"
-)
-
-var tmpl *template.Template
-
-func init() {
-	tmpl = template.Must(template.ParseGlob("templates/*"))
-}
-
 type Component struct {
 	Type     string       `json:"type"`
 	Children []*Component `json:"children"`
@@ -19,14 +7,6 @@ type Component struct {
 
 func (c *Component) Empty() bool {
 	return c.Type == "" && len(c.Children) == 0
-}
-
-func (c *Component) Render() string {
-	buffer := &bytes.Buffer{}
-
-	tmpl.ExecuteTemplate(buffer, strings.ToLower(c.Type)+".gohtml", c)
-
-	return buffer.String()
 }
 
 const (
--- template_renderer.go
+++ template_renderer.go
@@ -1,12 +1,38 @@
 package templatemanager
 
+import (
+	"bytes"
+	"strings"
+	"text/template"
+)
+
 type TemplateRenderer struct {
+	tmpl *template.Template
+}
+
+func init() {
 }
 
 func NewTemplateRenderer() *TemplateRenderer {
-	return &TemplateRenderer{}
+	r := &TemplateRenderer{}
+	r.loadTemplates()
+	return r
 }
 
-func (r *TemplateRenderer) Render(component *Component) string {
-	return component.Render()
+func (r *TemplateRenderer) Render(c *Component) string {
+	buffer := &bytes.Buffer{}
+	r.tmpl.ExecuteTemplate(buffer, strings.ToLower(c.Type)+".gohtml", c)
+	return buffer.String()
+}
+
+func (r *TemplateRenderer) loadTemplates() {
+	r.tmpl = template.Must(
+		template.New("").Funcs(r.funcMap()).ParseGlob("templates/*"),
+	)
+}
+
+func (r *TemplateRenderer) funcMap() template.FuncMap {
+	return template.FuncMap{
+		"Render": r.Render,
+	}
 }
