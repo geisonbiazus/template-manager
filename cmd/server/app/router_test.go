6ed173812db9c1551f7b01f22bbec8090f796a4e
Geison Biazus <geisonbiazus@gmail.com>
2018-07-02 20:52:17 -0300

    Add create template integration test

--- cmd/server/app/router_test.go
+++ cmd/server/app/router_test.go
@@ -12,14 +12,43 @@ import (
 )
 
 func TestMux(t *testing.T) {
-	server := httptest.NewServer(Mux("../../../" + templatemanager.DefaultTemplatePath))
+	type fixture struct {
+		server *httptest.Server
+	}
 
-	body := bytes.NewBufferString(`{"template": {"type":"Page"}}`)
-	res, _ := http.Post(server.URL+"/render_by_json", "application/json", body)
+	setup := func() *fixture {
+		server := httptest.NewServer(Mux("../../../" + templatemanager.DefaultTemplatePath))
 
-	assert.Equal(t, http.StatusOK, res.StatusCode)
+		return &fixture{
+			server: server,
+		}
+	}
 
-	expected := `{"html":"\u003c!DOCTYPE html\u003e\n\u003chtml\u003e\n\u003chead\u003e\n\u003cmeta charset=\"UTF-8\"\u003e\n\u003ctitle\u003e\u003c/title\u003e\n\u003c/head\u003e\n\u003cbody\u003e\n  \n\u003c/body\u003e\n\u003c/html\u003e\n"}` + "\n"
+	t.Run("/render_by_json", func(t *testing.T) {
+		f := setup()
+		res := doPost(f.server, "/render_by_json", `{"template": {"type":"Page"}}`)
+		expected := `{"html":"\u003c!DOCTYPE html\u003e\n\u003chtml\u003e\n\u003chead\u003e\n\u003cmeta charset=\"UTF-8\"\u003e\n\u003ctitle\u003e\u003c/title\u003e\n\u003c/head\u003e\n\u003cbody\u003e\n  \n\u003c/body\u003e\n\u003c/html\u003e\n"}` + "\n"
+		assertResponse(t, res, http.StatusOK, expected)
+	})
+
+	t.Run("POST /templates", func(t *testing.T) {
+		t.SkipNow()
+		f := setup()
+		res := doPost(f.server, "/templates", `{"template": {"body": {"type":"Page"}}}`)
+		expected := `{"template": {"id": "1", "body": {"type":"Page"}}}` + "\n"
+		assertResponse(t, res, http.StatusCreated, expected)
+	})
+}
+
+func doPost(s *httptest.Server, path, body string) *http.Response {
+	buffer := bytes.NewBufferString(body)
+	res, _ := http.Post(s.URL+path, "application/json", buffer)
+	return res
+}
+
+func assertResponse(t *testing.T, res *http.Response, status int, expectedBody string) {
+	t.Helper()
+	assert.Equal(t, status, res.StatusCode)
 	resBody, _ := ioutil.ReadAll(res.Body)
-	assert.Equal(t, expected, string(resBody))
+	assert.Equal(t, expectedBody, string(resBody))
 }
